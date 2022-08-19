8700d74864fd997215917dd5135010345e32d9cc
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-19 17:48:58 +0200

    Styles add

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `<app-server></app-server>
+  <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
@@ -13,3 +16,4 @@ export class ServersComponent implements OnInit {
   }
 
 }
+
