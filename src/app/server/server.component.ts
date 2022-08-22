7883c43f372ca875088865577ee961865951e608
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-22 07:34:40 +0200

    ngStyle - dynamically styling

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,12 +5,17 @@ import {Component} from "@angular/core";
   templateUrl: "./server.component.html"
 })
 export class ServerComponent {
-serverId: number= 10;
-serverStatus: string = "offline"
+  serverId: number= 10;
+  serverStatus: string = "offline"
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+  }
   getServerStatus() {
-
   return this.serverStatus
-
+  }
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red'
   }
 }
 
