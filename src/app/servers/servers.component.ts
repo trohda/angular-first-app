f8c0988471e4879efbb212bade333a5404a72957
Tomasz Rohda <tomaszrohda@wp.pl>
2022-08-23 16:44:24 +0200

    update

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus= "No server was created!";
   serverName="Test server";
   serverCreated = false;
+  servers= ['TestServer', 'TestServer2'];
 
   constructor() {
     setTimeout(()=> {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
   }
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = `Server was created! Name is ` + this.serverName;
   }
   onUpdateServerName(event: any) {
